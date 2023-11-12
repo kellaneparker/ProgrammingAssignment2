0cc3e04779ec473bcad54dad059f24f3bb225d83
Kellan Parker <109104541+kellaneparker@users.noreply.github.com>
2023-11-12 12:05:55 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,7 @@
 #_______________________________________________________________________________
 #Matrix inversion is usually a costly computation and there may be some benefit
 #to caching the inverse of a matrix rather than compute it repeatedly (there are
-#also alteratives to matrix inverion that we will not discuss here). Your 
+#also alternatives to matrix inverion that we will not discuss here). Your 
 #assignment is to write a pair of functions that cache the inverse of a matrix. 
 
 #Write the following functions:
@@ -13,7 +13,7 @@
 # 2. "cacheSolve": This function computes the inverse of the special "matrix" 
 #     returned by "makeCacheMatrix" above. If the inverse has already been
 #     calculated (and the matrix has not changed), then "cacheSolve" should 
-#     retrieve the inverse from the cache/ 
+#     retrieve the inverse from the cache. 
 #Computing the inverse of a square matrix can be done with the "solve" function
 #in R. For example, if "x" is a square invertible matrix, then "solve(x)" should
 #retrieve the inverse from the cache. 
